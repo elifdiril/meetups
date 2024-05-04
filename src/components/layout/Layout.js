67c9e10a0c3b12aa652802db7f85999ee3c3b245
Elif Gedik <elifdiril6@gmail.com>
2024-05-04 18:23:43 +0300

    loader added to layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import MainNavigation from './MainNavigation';
+import Loader from '../ui/Loader';
+import { useRouter } from 'next/router';
 
 function Layout(props) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+    const handleError = () => setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleError);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleError);
+    };
+  }, [router.events]);
+
   return (
     <div className='min-h-screen'>
       <MainNavigation />
+      {loading && <Loader />}
       <main className="w-11/12">{props.children}</main>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
-import { Inter } from "next/font/google";
 import { Fragment } from "react";
 import Head from "next/head";
 import MeetupList from "../components/meetup/MeetupList";
 import { MongoClient } from "mongodb";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
   return (
@@ -22,7 +19,6 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  // fetch data from an API
   const client = await MongoClient.connect(
     'mongodb+srv://elif:test123@cluster0.ehz8iuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   );
